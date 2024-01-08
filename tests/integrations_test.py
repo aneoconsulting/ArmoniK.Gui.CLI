fe04d45b07321e52972c5c578a4e55ac4d427d28
jeremyzyngeraneo <jzynger@aneo.fr>
2024-01-08 17:41:55 +0100

    tests: add integration tests for CLI

--- tests/integrations_test.py
+++ tests/integrations_test.py
@@ -6,7 +6,7 @@ import pytest
 # from .conftest import all_rpc_called, rpc_called, get_client
 import armonik_cli.admin as admin
 from armonik.client import ArmoniKResults, ArmoniKSubmitter, ArmoniKTasks, ArmoniKSessions
-from armonik.common import Session, TaskOptions
+from armonik.common import Session, Task, TaskOptions
 from armonik.common.enumwrapper import SessionStatus
 from armonik.common.enumwrapper import TASK_STATUS_ERROR, TASK_STATUS_CREATING , SESSION_STATUS_RUNNING, SESSION_STATUS_CANCELLED, SESSION_STATUS_UNSPECIFIED
 from armonik.common.filter import Filter
@@ -158,13 +158,6 @@ def all_rpc_called(service_name: str, missings: List[str] = [], endpoint: str = 
         return False
     return True
 
-def test_list_session_no_filter():
-    sessions_client: ArmoniKSessions = get_client("Sessions")
-    num, sessions = sessions_client.list_sessions()
-    assert rpc_called("Sessions", "ListSessions")
-    # TODO: Mock must be updated to return something and so that changes the following assertions
-    assert num == 0
-    assert sessions == []
 
 def test_create_session():
     sessions_client: ArmoniKSessions = get_client("Sessions")
@@ -178,13 +171,63 @@ def test_create_session():
     assert rpc_called("Sessions", "CreateSession")
     assert session_id == "session-id"
 
-def test_list_running_sessions():
-    sessions_client: ArmoniKSessions = get_client("Sessions")
-    sessions = admin.list_sessions(sessions_client, SessionFieldFilter.STATUS == SessionStatus.RUNNING)
+    # TODO: Mock must be updated to return something and so that changes the following assertions (empty [])
 
-    assert rpc_called("Sessions", "ListSessions", 2)
-    # TODO: Mock must be updated to return something and so that changes the following assertions
+def test_list_sessions_all():
+    sessions = admin.list_sessions(admin.create_session_client(grpc_endpoint), None)
+    assert rpc_called("Sessions", "ListSessions", 1)
     assert sessions == []
+
+def test_list_sessions_running():
+    sessions = admin.list_sessions(admin.create_session_client(grpc_endpoint), SessionFieldFilter.STATUS == SessionStatus.RUNNING)
+    assert rpc_called("Sessions", "ListSessions", 2)
+    assert sessions == []
+
+def test_list_sessions_cancelled():
+    sessions = admin.list_sessions(admin.create_session_client(grpc_endpoint), SessionFieldFilter.STATUS == SessionStatus.CANCELLED)
+    assert rpc_called("Sessions", "ListSessions", 3)
+    assert sessions == []
+
+def test_cancel_single_session():
+    admin.cancel_sessions(admin.create_session_client(grpc_endpoint), ["session-id"] )
+    assert rpc_called("Sessions", "CancelSession")
+
+def test_cancel_multiple_sessions():
+    admin.cancel_sessions(admin.create_session_client(grpc_endpoint), ["session-id1","session-id2","session-id3"] )
+    assert rpc_called("Sessions", "CancelSession", 4)
+
+def test_list_tasks_all():
+    tasks = admin.list_tasks(admin.create_task_client(grpc_endpoint), admin.create_task_filter("session-id", True, False, False) )
+    assert rpc_called("Tasks", "ListTasksDetailed")
+    assert tasks == []
+
+def test_list_tasks_creating():
+    tasks = admin.list_tasks(admin.create_task_client(grpc_endpoint), admin.create_task_filter("session-id", True, False, False) )
+    assert rpc_called("Tasks", "ListTasksDetailed", 2)
+    assert tasks == []
+
+def test_list_tasks_error():
+    tasks = admin.list_tasks(admin.create_task_client(grpc_endpoint), admin.create_task_filter("session-id", True, False, False) )
+    assert rpc_called("Tasks", "ListTasksDetailed", 3)
+    assert tasks == []
+
+def test_check_single_task():
+    task = admin.check_task(admin.create_task_client(grpc_endpoint), ["task-id"])
+    assert rpc_called("Tasks", "GetTask")
+    assert task[0].id == "task-id"
+
+def test_check_multiple_task():
+    task = admin.check_task(admin.create_task_client(grpc_endpoint), ["task-id", "task-id", "task-id"])
+    assert rpc_called("Tasks", "GetTask", 4)
+    assert task[0].id == "task-id"
+    assert task[1].id == "task-id"
+    assert task[2].id == "task-id"
+
+def test_check_non_existing_task():
+    task = admin.check_task(admin.create_task_client(grpc_endpoint), ["non-existing-id"])
+    assert rpc_called("Tasks", "GetTask", 5)
+    assert task == []
+
 
 # endpoint = 'http://localhost:5000/calls.json'  
 # reset_endpoint = "http://localhost:5000/reset"
