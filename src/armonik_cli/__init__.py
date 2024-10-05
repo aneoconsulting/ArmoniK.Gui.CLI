58a44df5b446ecbbc2ab4e6b36b826609fbf7969
qdelamea <qdelamea@aneo.fr>
2024-10-05 16:07:59 +0200

    feat: add command to list sessions

--- src/armonik_cli/__init__.py
+++ src/armonik_cli/__init__.py
@@ -1,6 +1,4 @@
 from armonik_cli.version import __version__
 
 
-__all__ = [
-    "__version__"
-]
+__all__ = ["__version__"]
--- src/armonik_cli/cli.py
+++ src/armonik_cli/cli.py
@@ -1,6 +1,11 @@
+import grpc
 import rich_click as click
 
+from armonik.client.sessions import ArmoniKSessions
+from rich.table import Table
+
 from armonik_cli import version
+from armonik_cli.console import console
 
 
 @click.group(name="armonik")
@@ -10,3 +15,51 @@ def cli():
     ArmoniK CLI is a tool to monitor and manage ArmoniK clusters.
     """
     pass
+
+
+@cli.group(name="sessions")
+def sessions():
+    """Manage cluster sessions."""
+    pass
+
+
+@sessions.command()
+@click.option(
+    "-e",
+    "--endpoint",
+    type=str,
+    required=True,
+    help="Endpoint of the cluster to connect to.",
+    metavar="ENDPOINT",
+)
+@click.option(
+    "-o",
+    "--output",
+    type=click.Choice(["fancy", "json"], case_sensitive=False),
+    default="fancy",
+    show_default=True,
+    help="Endpoint of the cluster to connect to.",
+    metavar="FORMAT",
+)
+def list(endpoint: str, output: str):
+    """List the sessions of an ArmoniK cluster."""
+    with grpc.insecure_channel(endpoint) as channel:
+        sessions_client = ArmoniKSessions(channel)
+        total, sessions = sessions_client.list_sessions()
+
+    if total > 0:
+        if output == "fancy":
+            table = Table(box=None)
+            table.add_column("ID")
+            table.add_column("Status")
+            table.add_column("Duration")
+
+            for session in sessions:
+                table.add_row(session.session_id, str(session.status), str(session.duration))
+
+            console.print(table)
+        else:
+            sessions = [s.__dict__ for s in sessions]
+            console.print(sessions)
+
+    console.print(f"\n{total} sessions found.")
