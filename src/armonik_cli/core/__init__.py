69eda789c77ec0d9b7ffb87c42d28dcbefd9680c
qdelamea <qdelamea@aneo.fr>
2024-11-07 23:09:17 +0100

    feat: add base_command decorator

--- src/armonik_cli/core/__init__.py
+++ src/armonik_cli/core/__init__.py
@@ -1,6 +1,6 @@
 from armonik_cli.core.console import console
-from armonik_cli.core.decorators import error_handler
+from armonik_cli.core.decorators import base_command
 from armonik_cli.core.params import KeyValuePairParam, TimeDeltaParam
 
 
-__all__ = ["error_handler", "KeyValuePairParam", "TimeDeltaParam", "console"]
+__all__ = ["base_command", "KeyValuePairParam", "TimeDeltaParam", "console"]
--- src/armonik_cli/core/decorators.py
+++ src/armonik_cli/core/decorators.py
@@ -8,7 +8,15 @@ from armonik_cli.exceptions import NotFoundError, InternalError
 
 
 def error_handler(func=None):
-    """A decorator to manage the correct display of errors.."""
+    """Decorator to ensure correct display of errors.
+    
+    Args:
+        func: The command function to be decorated. If None, a partial function is returned,
+            allowing the decorator to be used with parentheses.
+
+    Returns:
+        The wrapped function with added CLI options.
+    """
     # Allow to call the decorator with parenthesis.
     if not func:
         return partial(error_handler)
@@ -32,5 +40,63 @@ def error_handler(func=None):
                 console.print_exception()
             else:
                 raise InternalError("An internal fatal error occured.")
+
+    return wrapper
+
+
+def base_command(func=None):
+    """Decorator to add common CLI options to a Click command function, including
+    'endpoint', 'output', and 'debug'. These options are automatically passed 
+    as arguments to the decorated function.
+
+    The following options are added to the command:
+    - `--endpoint` (required): Specifies the cluster endpoint.
+    - `--output`: Sets the output format, with options 'yaml', 'json', or 'table' (default is 'json').
+    - `--debug`: Enables debug mode, printing additional logs if set.
+
+    Warning:
+        If the decorated function has parameters with the same names as the options added by 
+        this decorator, this can lead to conflicts and unpredictable behavior.
+
+    Args:
+        func: The command function to be decorated. If None, a partial function is returned,
+            allowing the decorator to be used with parentheses.
+
+    Returns:
+        The wrapped function with added CLI options.
+    """
+
+    # Allow to call the decorator with parenthesis.
+    if not func:
+        return partial(base_command)
+
+    # Define the wrapper function with added Click options
+    @click.option(
+        "-e",
+        "--endpoint",
+        type=str,
+        required=True,
+        help="Endpoint of the cluster to connect to.",
+        metavar="ENDPOINT",
+    )
+    @click.option(
+        "-o",
+        "--output",
+        type=click.Choice(["yaml", "json", "table"], case_sensitive=False),
+        default="json",
+        show_default=True,
+        help="Commands output format.",
+        metavar="FORMAT",
+    )
+    @click.option(
+        "--debug", is_flag=True, default=False, help="Print debug logs and internal errors."
+    )
+    @error_handler
+    @wraps(func)
+    def wrapper(endpoint: str, output: str, debug: bool, *args, **kwargs):
+        kwargs["endpoint"] = endpoint
+        kwargs["output"] = output
+        kwargs["debug"] = debug
+        return func(*args, **kwargs)
 
     return wrapper
